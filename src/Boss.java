883a8f6c421dba76d0420adf69ce4cbc60bb4eb9
Minas-Theodoros Charakopoulos <ics20072@uom.edu.gr>
2021-04-28 01:38:01 +0300

    Addition Class "Boss" added with field form super class "User"

---
+++ src/Boss.java
@@ -0,0 +1,10 @@
+
+public class Boss extends User{
+
+	public Boss(String firstName, String lastName, String username, String password, String email, String telephone,
+			String address, String gender, String birthday, String companyPost, String image, Company myCompany) {
+		super(firstName, lastName, username, password, email, telephone, address, gender, birthday, companyPost, image,
+				myCompany);
+	}
+
+}
