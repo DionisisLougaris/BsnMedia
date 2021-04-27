05eb022ceb6d03adc255d4dd979de932a36813e0
Dionisis <dionisisloug@gmail.com>
2021-04-28 01:22:06 +0300

    Additions: Fields for User class and Constructor Post class Notification class

--- src/User.java
+++ src/User.java
@@ -1,4 +1,41 @@
+import java.util.ArrayList;
 
 abstract public class User {
-	private String name;
+	
+	protected String firstName;
+	protected String lastName;
+	protected String username;
+	protected String password;
+	protected String email;
+	protected String Telephone;
+	protected String address;
+	protected String gender;
+	protected String birthday;
+	protected String companyPost;
+	protected String image;
+	protected ArrayList<User> listOfConnections = new ArrayList<User>();
+	protected ArrayList<Post> listOfPosts = new ArrayList<Post>();
+	protected Company myCompany;
+	protected ArrayList<Notification> listOfNotifications = new ArrayList<Notification>();
+	protected ArrayList<User> pendingConnectionRequests = new ArrayList<User>();
+	
+	public User(String firstName, String lastName, String username, String password, String email, String telephone,
+			String address, String gender, String birthday, String companyPost, String image, Company myCompany) {
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		Telephone = telephone;
+		this.address = address;
+		this.gender = gender;
+		this.birthday = birthday;
+		this.companyPost = companyPost;
+		this.image = image;
+		this.myCompany = myCompany;
+		
+	}
+	
+	
 }
