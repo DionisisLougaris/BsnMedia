ad456803f8187c6d0402236a02d643097cc764fd
Minas-Theodoros Charakopoulos <ics20072@uom.edu.gr>
2021-05-03 00:18:41 +0300

    Edit constructor on "Account" class.

--- src/Account.java
+++ src/Account.java
@@ -8,11 +8,12 @@ public class Account {
 	
 	
 	//Constructor for Account
-	public Account(String username, String email, Company myCompany) {
+	public Account(String username, String email, Company myCompany, Password myPassword) {
 		
 		this.username = username;
 		this.email = email;
 		this.myCompany = myCompany;
+		this.myPassword = myPassword;
 	}
 
 }
