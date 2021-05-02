70fb0c76b2659203effd04563f995704bf34acdb
EleniMaz <eleniuni55@gmail.com>
2021-05-03 00:30:29 +0300

    Added array list of company members in class company.

--- src/Company.java
+++ src/Company.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 
 public class Company {
 	
 	private String name;
 	private String info;
+	private ArrayList<User> companyMembers = new ArrayList<>();
 	
 	public Company(String name, String info) {
 		this.name = name;
