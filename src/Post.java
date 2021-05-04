27ff356a694be82cd3d1a6a2f391bae9194591fc
Minas-Theodoros Charakopoulos <ics20072@uom.edu.gr>
2021-05-04 04:26:01 +0300

    Additions: new field and an ArrayList added in Post class. addLike() method added to add a like and a liker to a Post.

--- src/Post.java
+++ src/Post.java
@@ -1,4 +1,8 @@
 import java.time.*;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class Post {
 	
@@ -6,6 +10,8 @@ public class Post {
 	private String content;
 	private String postScope; //Εμβελεια
 	private LocalDateTime timestamp;
+	private int numberOfLikes;
+	private ArrayList<User> likers = new ArrayList<User>(); //All users who likes the post
 	
 	
 	//Constructor for Post class
@@ -14,6 +20,26 @@ public class Post {
 		this.content = content;
 		this.postScope = postScope;
 		this.timestamp = LocalDateTime.now();
+		this.numberOfLikes = 0; //initially the number of Likes is 0
 	}
+	
+	
+	/*method which adds Like to a Post
+	Boolean to know if in Gui we will change the value with the Likes so that unnecessary procedures are not done in Gui*/
+	public boolean addLike(User theUser) {
+		
+		if (likers.contains(theUser)) {
+			String message = "You have already like this Post!";
+			JOptionPane.showMessageDialog(new JFrame(), message, "Message",
+			        JOptionPane.INFORMATION_MESSAGE);
+			return false; //Like couldn' t added
+		}
+		else {
+			numberOfLikes++;
+			likers.add(theUser);
+			return true; //New like added
+		}	
+	}
+	
 	
 }
