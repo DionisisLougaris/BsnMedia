23330cb421ff0a80483a450bcb9715601eb24cf5
Minas-Theodoros Charakopoulos <ics20072@uom.edu.gr>
2021-05-08 03:38:05 +0300

    Change constructor in Notification class. Add static method "sendFromGmail". We need to add the libary javax.mail to run the method.

--- src/Help.java
+++ src/Help.java
@@ -1,3 +1,12 @@
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class Help {
 	
@@ -5,6 +14,7 @@ public class Help {
 	private String authorEmail;
 	private String question;
 	private String supportEmail;
+	private String supportEmailPassword;
 	
 	
 	//Constructor for Help class;
@@ -14,19 +24,57 @@ public class Help {
 		this.authorEmail = authorEmail;
 		this.question = question;
 		this.supportEmail = "itintelligenceuom@gmail.com";
+		this.supportEmailPassword = "ITintelligence2001";
 	}
 	
 	
-	//Method to send Email to the Bsn Media Support
+	//A method that allows users to contact the Bsn Media technical support team.
 	public void sendQuestion() {
 		
 	}
 	
-	public static void sendEmail(String emailSender, String emailReceiver, String passwordReceiver, String emailthem) {
-		
-	}
 	
-	
+	//A method that sends email to someone
+	 private static void sendFromGMail(String from, String pass, String[] to, String subject, String body) {
+	        Properties props = System.getProperties();
+	        String host = "smtp.gmail.com";
+	        props.put("mail.smtp.starttls.enable", "true");
+	        props.put("mail.smtp.host", host);
+	        props.put("mail.smtp.user", from);
+	        props.put("mail.smtp.password", pass);
+	        props.put("mail.smtp.port", "587");
+	        props.put("mail.smtp.auth", "true");
+
+	        Session session = Session.getDefaultInstance(props);
+	        MimeMessage message = new MimeMessage(session);
+
+	        try {
+	            message.setFrom(new InternetAddress(from));
+	            InternetAddress[] toAddress = new InternetAddress[to.length];
+
+	            // To get the array of addresses
+	            for( int i = 0; i < to.length; i++ ) {
+	                toAddress[i] = new InternetAddress(to[i]);
+	            }
+
+	            for( int i = 0; i < toAddress.length; i++) {
+	                message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+	            }
+
+	            message.setSubject(subject);
+	            message.setText(body);
+	            Transport transport = session.getTransport("smtp");
+	            transport.connect(host, from, pass);
+	            transport.sendMessage(message, message.getAllRecipients());
+	            transport.close();
+	        }
+	        catch (AddressException ae) {
+	            ae.printStackTrace();
+	        }
+	        catch (MessagingException me) {
+	            me.printStackTrace();
+	        }
+	    }
 	
 
 }
--- src/Notification.java
+++ src/Notification.java
@@ -4,12 +4,36 @@ public class Notification {
 	
 	private String notificationContent;
 	private LocalDateTime timestamp;
+	private Group aboutThisGroup;
+	private User aboutThisUser;
+	private Conversation aboutThisConversation;
 	
 	
-	//Constructor for Notification class
-	public Notification(String notificationContent) {
+	//Constructor for Notification class when a notification is about Group
+	public Notification(String notificationContent, Group aboutThisGroup) {
 		this.notificationContent = notificationContent;
 		this.timestamp = LocalDateTime.now();
+		this.aboutThisGroup = aboutThisGroup;
+		this.aboutThisUser = null;
+		this.aboutThisConversation = null;
+	}
+	
+	//Constructor for Notification class when a notification is about User
+	public Notification(String notificationContent, User aboutThisUser) {
+		this.notificationContent = notificationContent;
+		this.timestamp = LocalDateTime.now();
+		this.aboutThisGroup = null;
+		this.aboutThisUser = aboutThisUser;
+		this.aboutThisConversation = null;
+	}
+	
+	//Constructor for Notification class when a notification is about a message
+	public Notification(String notificationContent, Conversation aboutThisConversation) {
+		this.notificationContent = notificationContent;
+		this.timestamp = LocalDateTime.now();
+		this.aboutThisGroup = null;
+		this.aboutThisUser = null;
+		this.aboutThisConversation = aboutThisConversation;
 	}
 	
 }
