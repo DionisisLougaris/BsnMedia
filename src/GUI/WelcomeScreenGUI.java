a36ecff285bf67a2521e77500ad8186e132fe999
EleniMaz <eleniuni55@gmail.com>
2021-05-19 10:35:28 +0300

    Deleted the classes from GUI package and added class WelcomeScreenGUI using window builder.

--- src/GUI/WelcomeScreenGUI.java
+++ src/GUI/WelcomeScreenGUI.java
@@ -1,5 +1,43 @@
 package GUI;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
 public class WelcomeScreenGUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					WelcomeScreenGUI window = new WelcomeScreenGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public WelcomeScreenGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 
 }
