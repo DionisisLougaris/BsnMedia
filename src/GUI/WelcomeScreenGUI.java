a0c38ce43acc479604cecb6de538629ff0dbb6e2
elenikosmidou <ics20064@uom.edu.gr>
2021-05-28 00:24:51 +0300

    Addition: ButtonListener to class WelcomeScreenGUI

--- src/GUI/WelcomeScreenGUI.java
+++ src/GUI/WelcomeScreenGUI.java
@@ -1,53 +1,30 @@
 package GUI;
 
-import java.awt.EventQueue;
+import java.awt.*;
+import java.awt.event.*;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.Color;
-import javax.swing.JTextField;
-import javax.swing.JPasswordField;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.border.BevelBorder;
-import javax.swing.border.LineBorder;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.*;
 
-public class WelcomeScreenGUI {
+
+
+
+public class WelcomeScreenGUI<Company> {
 
 	JFrame frame;
-	private JTextField txtUsername;
+	private JTextField usernameField;
 	private JPasswordField passwordField;
+	private Company theCompany;
+	private JButton logInButton;
+	private JButton forgotPasswordButton;
+	private JButton newAccountButton;
+	private JButton helpButton;
+	private JPanel panel;
+	
+	
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					WelcomeScreenGUI window = new WelcomeScreenGUI();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 */
-	public WelcomeScreenGUI() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
+	
+	public WelcomeScreenGUI(Company aCompany) {
+		theCompany=aCompany;
 		frame = new JFrame();
 		frame.getContentPane().setBackground(new Color(153, 204, 204));
 		frame.setBounds(100, 100, 909, 630);
@@ -61,44 +38,38 @@ public class WelcomeScreenGUI {
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 		
-		txtUsername = new JTextField();
-		txtUsername.setForeground(new Color(0, 51, 153));
-		txtUsername.setBackground(new Color(255, 153, 0));
-		txtUsername.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		txtUsername.setText("          Username");
-		txtUsername.setBounds(134, 158, 179, 33);
-		panel.add(txtUsername);
-		txtUsername.setColumns(10);
+		usernameField = new JTextField();
+		usernameField.setForeground(new Color(0, 51, 153));
+		usernameField.setBackground(new Color(255, 153, 0));
+		usernameField.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		usernameField.setText("          Username");
+		usernameField.setBounds(134, 158, 179, 33);
+		panel.add(usernameField);
+		usernameField.setColumns(10);
 		
 		passwordField = new JPasswordField();
 		passwordField.setBounds(134, 224, 179, 33);
 		panel.add(passwordField);
 		
-		JButton btnNewButton = new JButton("Log In");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnNewButton.setBounds(183, 289, 97, 25);
-		panel.add(btnNewButton);
+		JButton logInButton = new JButton("Log In");
 		
-		JButton btnNewButton_1 = new JButton("Forgot Password?");
-		btnNewButton_1.setForeground(new Color(0, 0, 0));
-		btnNewButton_1.setBackground(new Color(255, 153, 0));
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton_1.setBounds(144, 467, 194, 25);
-		panel.add(btnNewButton_1);
+		logInButton.setBounds(183, 289, 97, 25);
+		panel.add(logInButton);
 		
-		JButton btnNewButton_2 = new JButton("Create Account");
-		btnNewButton_2.setBounds(169, 327, 132, 25);
-		panel.add(btnNewButton_2);
+		JButton forgotPasswordButton = new JButton("Forgot Password?");
+		forgotPasswordButton.setForeground(new Color(0, 0, 0));
+		forgotPasswordButton.setBackground(new Color(255, 153, 0));
 		
-		JButton btnNewButton_3 = new JButton("Help");
-		btnNewButton_3.setBounds(340, 545, 97, 25);
-		panel.add(btnNewButton_3);
+		forgotPasswordButton.setBounds(144, 467, 194, 25);
+		panel.add(forgotPasswordButton);
+		
+		JButton newAccountButton = new JButton("Create Account");
+		newAccountButton.setBounds(169, 327, 132, 25);
+		panel.add(newAccountButton);
+		
+		JButton helpButton = new JButton("Help");
+		helpButton.setBounds(340, 545, 97, 25);
+		panel.add(helpButton);
 		
 		JLabel lblNewLabel = new JLabel("Welcome to BSN Media!");
 		lblNewLabel.setBounds(83, 47, 285, 27);
@@ -109,5 +80,38 @@ public class WelcomeScreenGUI {
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		lblNewLabel_1.setBounds(133, 87, 207, 16);
 		frame.getContentPane().add(lblNewLabel_1);
+		
+		ButtonListener listener=new ButtonListener();
+		logInButton.addActionListener(listener);
+		newAccountButton.addActionListener(listener);
+		forgotPasswordButton.addActionListener(listener);
+		helpButton.addActionListener(listener);
+	}
+	
+	class ButtonListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			if(e.getSource()==logInButton)
+			{
+				String username= usernameField.getText();
+				String password= passwordField.getPassword();  //mallon prepei anagkastika na to kanoume char giati to bgazei error
+				
+				theCompany.isCompanyMember(username).getMyAccount().loginAttempt(username,password);
+				
+			}
+			else if(e.getSource()==newAccountButton)
+			{
+				new CreateAccountGUI(); //thelei parametro company
+			}
+			else if(e.getSource()==forgotPasswordButton)
+			{
+				new ForgotPasswordGUI();  //thelei parametro company
+			}
+			else if(e.getSource()==helpButton)
+			{
+				new HelpGUI();
+			}
+		}
 	}
 }
