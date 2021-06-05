3ae67629c7db94137013b53b1b1ef7270db13855
Minas-Theodoros Charakopoulos <ics20072@uom.edu.gr>
2021-06-05 05:31:31 +0300

    Aesthetic changes were made to the GUIs, CreateAccountGUI and ForgotPasswordGUI.

--- src/GUI/CreateAccountGUI.java
+++ src/GUI/CreateAccountGUI.java
@@ -3,6 +3,8 @@ package GUI;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 import javax.swing.JFrame;
@@ -17,14 +19,23 @@ import entities.User;
 
 import java.awt.Color;
 import java.awt.Component;
+
+import javax.imageio.ImageIO;
 import javax.swing.Box;
 import javax.swing.ButtonGroup;
 import javax.swing.ButtonModel;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
 import javax.swing.JCheckBox;
 import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.Image;
+
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.border.LineBorder;
 
 public class CreateAccountGUI {
 
@@ -53,7 +64,7 @@ public class CreateAccountGUI {
 
 	private void initialize(Company aCompany) {
 		frame = new JFrame();
-		frame.getContentPane().setBackground(new Color(255, 204, 153));
+		frame.getContentPane().setBackground(new Color(255, 153, 102));
 		frame.setBounds(100, 100, 541, 636);
 		frame.getContentPane().setLayout(null);
 		frame.setVisible(true);
@@ -62,97 +73,148 @@ public class CreateAccountGUI {
 		theCompany = aCompany;
 		
 		txtCompanyCode = new JTextField();
+		txtCompanyCode.setBorder(null);
+		txtCompanyCode.setBackground(new Color(255, 153, 102));
+		txtCompanyCode.setForeground(new Color(255, 255, 255));
 		txtCompanyCode.setText("Company Code");
-		txtCompanyCode.setBounds(202, 27, 116, 22);
+		txtCompanyCode.setBounds(289, 137, 116, 16);
 		frame.getContentPane().add(txtCompanyCode);
 		txtCompanyCode.setColumns(10);
 		
 		
 		
 		txtUsername = new JTextField();
+		txtUsername.setBorder(null);
+		txtUsername.setBackground(new Color(255, 153, 102));
+		txtUsername.setForeground(new Color(255, 255, 255));
 		txtUsername.setText("Username");
-		txtUsername.setBounds(202, 62, 116, 22);
+		txtUsername.setBounds(289, 164, 116, 16);
 		frame.getContentPane().add(txtUsername);
 		txtUsername.setColumns(10);
 		
 		txtPassword = new JTextField();
+		txtPassword.setBorder(null);
+		txtPassword.setBackground(new Color(255, 153, 102));
+		txtPassword.setForeground(new Color(255, 255, 255));
 		txtPassword.setText("Password");
-		txtPassword.setBounds(202, 97, 116, 22);
+		txtPassword.setBounds(289, 191, 116, 22);
 		frame.getContentPane().add(txtPassword);
 		txtPassword.setColumns(10);
 		
 		txtComfirmPassword = new JTextField();
+		txtComfirmPassword.setBorder(null);
+		txtComfirmPassword.setBackground(new Color(255, 153, 102));
+		txtComfirmPassword.setForeground(new Color(255, 255, 255));
 		txtComfirmPassword.setText("Comfirm  Password");
-		txtComfirmPassword.setBounds(202, 132, 116, 22);
+		txtComfirmPassword.setBounds(289, 220, 116, 22);
 		frame.getContentPane().add(txtComfirmPassword);
 		txtComfirmPassword.setColumns(10);
 		
 		txtEmail = new JTextField();
+		txtEmail.setBorder(null);
+		txtEmail.setBackground(new Color(255, 153, 102));
+		txtEmail.setForeground(new Color(255, 255, 255));
 		txtEmail.setText("Email");
-		txtEmail.setBounds(202, 167, 116, 22);
+		txtEmail.setBounds(289, 245, 116, 22);
 		frame.getContentPane().add(txtEmail);
 		txtEmail.setColumns(10);
 		
 		txtTelephone = new JTextField();
+		txtTelephone.setBorder(null);
+		txtTelephone.setBackground(new Color(255, 153, 102));
+		txtTelephone.setForeground(new Color(255, 255, 255));
 		txtTelephone.setText("Telephone");
-		txtTelephone.setBounds(202, 202, 116, 22);
+		txtTelephone.setBounds(289, 272, 116, 22);
 		frame.getContentPane().add(txtTelephone);
 		txtTelephone.setColumns(10);
 		
 		txtFirstName = new JTextField();
+		txtFirstName.setBorder(null);
+		txtFirstName.setBackground(new Color(255, 153, 102));
+		txtFirstName.setForeground(new Color(255, 255, 255));
 		txtFirstName.setText("First Name");
-		txtFirstName.setBounds(202, 237, 116, 22);
+		txtFirstName.setBounds(289, 305, 73, 22);
 		frame.getContentPane().add(txtFirstName);
 		txtFirstName.setColumns(10);
 		
 		txtLastName = new JTextField();
+		txtLastName.setBorder(null);
+		txtLastName.setBackground(new Color(255, 153, 102));
+		txtLastName.setForeground(new Color(255, 255, 255));
 		txtLastName.setText("Last Name");
-		txtLastName.setBounds(202, 272, 116, 22);
+		txtLastName.setBounds(289, 332, 116, 22);
 		frame.getContentPane().add(txtLastName);
 		txtLastName.setColumns(10);
 		
 		txtAddress = new JTextField();
+		txtAddress.setBorder(null);
+		txtAddress.setBackground(new Color(255, 153, 102));
+		txtAddress.setForeground(new Color(255, 255, 255));
 		txtAddress.setText("Address");
-		txtAddress.setBounds(202, 307, 116, 22);
+		txtAddress.setBounds(289, 359, 88, 22);
 		frame.getContentPane().add(txtAddress);
 		txtAddress.setColumns(10);
 		
 		txtGender = new JTextField();
+		txtGender.setBorder(null);
+		txtGender.setBackground(new Color(255, 153, 102));
+		txtGender.setForeground(new Color(255, 255, 255));
 		txtGender.setText("Gender");
-		txtGender.setBounds(202, 342, 116, 22);
+		txtGender.setBounds(289, 386, 73, 22);
 		frame.getContentPane().add(txtGender);
 		txtGender.setColumns(10);
 		
 		txtBirthday = new JTextField();
+		txtBirthday.setBorder(null);
+		txtBirthday.setBackground(new Color(255, 153, 102));
+		txtBirthday.setForeground(new Color(255, 255, 255));
 		txtBirthday.setText("Birthday");
-		txtBirthday.setBounds(202, 377, 116, 22);
+		txtBirthday.setBounds(289, 413, 116, 22);
 		frame.getContentPane().add(txtBirthday);
 		txtBirthday.setColumns(10);
 		
 		txtSpeciality = new JTextField();
+		txtSpeciality.setBorder(null);
+		txtSpeciality.setBackground(new Color(255, 153, 102));
+		txtSpeciality.setForeground(new Color(255, 255, 255));
 		txtSpeciality.setText("Speciality");
-		txtSpeciality.setBounds(202, 412, 116, 22);
+		txtSpeciality.setBounds(289, 440, 88, 22);
 		frame.getContentPane().add(txtSpeciality);
 		txtSpeciality.setColumns(10);
 		
-		JLabel lblNewLabel = new JLabel("Company Post");
-		lblNewLabel.setBounds(133, 452, 88, 16);
+		JLabel lblNewLabel = new JLabel("Company Post:");
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+		lblNewLabel.setBounds(177, 470, 88, 16);
 		frame.getContentPane().add(lblNewLabel);
 		
 		createAccountButton = new JButton("Create New Account");
+		createAccountButton.setBackground(new Color(255, 153, 102));
+		createAccountButton.setBorder(new LineBorder(new Color(255, 255, 255)));
+		createAccountButton.setForeground(new Color(255, 255, 255));
 		createAccountButton.setBounds(177, 515, 169, 25);
 		frame.getContentPane().add(createAccountButton);
 		
 		loginButton = new JButton("Log In");
+		loginButton.setBackground(new Color(255, 153, 102));
+		loginButton.setBorder(new LineBorder(new Color(255, 255, 255)));
+		loginButton.setForeground(new Color(255, 255, 255));
 		loginButton.setBounds(385, 515, 97, 25);
 		frame.getContentPane().add(loginButton);
 		
 		JRadioButton rdbtnEmployee= new JRadioButton("Employee");
-		rdbtnEmployee.setBounds(219, 448, 88, 25);
+		rdbtnEmployee.setFont(new Font("Tahoma", Font.ITALIC, 11));
+		rdbtnEmployee.setForeground(new Color(255, 255, 255));
+		rdbtnEmployee.setBorder(null);
+		rdbtnEmployee.setBackground(new Color(255, 153, 102));
+		rdbtnEmployee.setBounds(289, 469, 73, 22);
 		frame.getContentPane().add(rdbtnEmployee);
 		
 		JRadioButton rdbtnChief= new JRadioButton("Chief");
-		rdbtnChief.setBounds(311, 448, 64, 25);
+		rdbtnChief.setFont(new Font("Tahoma", Font.ITALIC, 11));
+		rdbtnChief.setForeground(new Color(255, 255, 255));
+		rdbtnChief.setBorder(null);
+		rdbtnChief.setBackground(new Color(255, 153, 102));
+		rdbtnChief.setBounds(364, 469, 64, 22);
 		frame.getContentPane().add(rdbtnChief);
 		
 		ButtonGroup radioGroup = new ButtonGroup();
@@ -160,56 +222,117 @@ public class CreateAccountGUI {
 		radioGroup.add(rdbtnChief);
 		
 		helpButton = new JButton("Help");
+		helpButton.setBackground(new Color(255, 153, 102));
+		helpButton.setBorder(new LineBorder(new Color(255, 255, 255)));
+		helpButton.setForeground(new Color(255, 255, 255));
 		helpButton.setBounds(385, 551, 97, 25);
 		frame.getContentPane().add(helpButton);
 		
 		JLabel lblNewLabel_1 = new JLabel("Company Code:");
-		lblNewLabel_1.setBounds(84, 30, 106, 16);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		lblNewLabel_1.setForeground(new Color(255, 255, 255));
+		lblNewLabel_1.setBounds(177, 140, 82, 16);
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Username:");
-		lblNewLabel_2.setBounds(84, 65, 73, 16);
+		lblNewLabel_2.setForeground(new Color(255, 255, 255));
+		lblNewLabel_2.setBounds(177, 167, 73, 16);
 		frame.getContentPane().add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_3 = new JLabel("Password:");
-		lblNewLabel_3.setBounds(84, 100, 82, 16);
+		lblNewLabel_3.setForeground(new Color(255, 255, 255));
+		lblNewLabel_3.setBounds(177, 194, 73, 16);
 		frame.getContentPane().add(lblNewLabel_3);
 		
-		JLabel lblNewLabel_4 = new JLabel("Confirm Password:");
-		lblNewLabel_4.setBounds(81, 135, 109, 16);
+		JLabel lblNewLabel_4 = new JLabel("Confirm Pass:");
+		lblNewLabel_4.setForeground(new Color(255, 255, 255));
+		lblNewLabel_4.setBounds(177, 223, 109, 16);
 		frame.getContentPane().add(lblNewLabel_4);
 		
 		JLabel lblNewLabel_5 = new JLabel("Email:");
-		lblNewLabel_5.setBounds(81, 170, 56, 16);
+		lblNewLabel_5.setForeground(new Color(255, 255, 255));
+		lblNewLabel_5.setBounds(177, 248, 56, 16);
 		frame.getContentPane().add(lblNewLabel_5);
 		
 		JLabel lblNewLabel_6 = new JLabel("Telephone:");
-		lblNewLabel_6.setBounds(78, 205, 88, 16);
+		lblNewLabel_6.setForeground(new Color(255, 255, 255));
+		lblNewLabel_6.setBounds(177, 275, 73, 16);
 		frame.getContentPane().add(lblNewLabel_6);
 		
 		JLabel lblNewLabel_7 = new JLabel("First Name:");
-		lblNewLabel_7.setBounds(78, 240, 88, 16);
+		lblNewLabel_7.setForeground(new Color(255, 255, 255));
+		lblNewLabel_7.setBounds(177, 308, 73, 16);
 		frame.getContentPane().add(lblNewLabel_7);
 		
 		JLabel lblNewLabel_8 = new JLabel("Last Name:");
-		lblNewLabel_8.setBounds(74, 275, 83, 16);
+		lblNewLabel_8.setForeground(new Color(255, 255, 255));
+		lblNewLabel_8.setBounds(177, 335, 83, 16);
 		frame.getContentPane().add(lblNewLabel_8);
 		
 		JLabel lblNewLabel_9 = new JLabel("Address:");
-		lblNewLabel_9.setBounds(75, 310, 82, 16);
+		lblNewLabel_9.setForeground(new Color(255, 255, 255));
+		lblNewLabel_9.setBounds(177, 362, 82, 16);
 		frame.getContentPane().add(lblNewLabel_9);
 		
 		JLabel lblNewLabel_10 = new JLabel("Gender:");
-		lblNewLabel_10.setBounds(74, 345, 56, 16);
+		lblNewLabel_10.setForeground(new Color(255, 255, 255));
+		lblNewLabel_10.setBounds(177, 389, 56, 16);
 		frame.getContentPane().add(lblNewLabel_10);
 		
 		JLabel lblNewLabel_11 = new JLabel("Birthday:");
-		lblNewLabel_11.setBounds(78, 380, 56, 16);
+		lblNewLabel_11.setForeground(new Color(255, 255, 255));
+		lblNewLabel_11.setBounds(177, 416, 56, 16);
 		frame.getContentPane().add(lblNewLabel_11);
 		
 		JLabel lblNewLabel_12 = new JLabel("Spaciality:");
-		lblNewLabel_12.setBounds(81, 415, 64, 16);
+		lblNewLabel_12.setForeground(new Color(255, 255, 255));
+		lblNewLabel_12.setBounds(177, 443, 64, 16);
 		frame.getContentPane().add(lblNewLabel_12);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(255, 255, 255));
+		panel.setBounds(0, 0, 535, 119);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_13 = new JLabel("");
+		BufferedImage imageicon;
+		try {
+			imageicon = ImageIO.read(new File("label_backgrounds/BSNlogo.jpg"));
+			ImageIcon image = new ImageIcon(imageicon);
+			Image imagerisize = image.getImage().getScaledInstance(100, 100, 80) ;
+			lblNewLabel_13.setIcon(new ImageIcon(imagerisize));
+		} catch (IOException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		lblNewLabel_13.setBounds(41, 11, 115, 97);
+		panel.add(lblNewLabel_13);
+		
+		JLabel lblNewLabel_14 = new JLabel("Create easy your Account!");
+		lblNewLabel_14.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 16));
+		lblNewLabel_14.setBounds(234, 57, 211, 29);
+		panel.add(lblNewLabel_14);
+		
+		JLabel lblNewLabel_14_1 = new JLabel("You are new here?");
+		lblNewLabel_14_1.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 16));
+		lblNewLabel_14_1.setBounds(250, 17, 179, 29);
+		panel.add(lblNewLabel_14_1);
+		
+		JSeparator separator = new JSeparator();
+		separator.setForeground(new Color(255, 255, 255));
+		separator.setBounds(0, 503, 535, 15);
+		frame.getContentPane().add(separator);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setForeground(new Color(255, 255, 255));
+		separator_1.setBounds(289, 163, 101, 2);
+		frame.getContentPane().add(separator_1);
+		
+		JSeparator separator_1_1 = new JSeparator();
+		separator_1_1.setForeground(Color.WHITE);
+		separator_1_1.setBounds(289, 184, 101, 2);
+		frame.getContentPane().add(separator_1_1);
 		
 		createAccountButton.addActionListener(new ActionListener() {
 
--- src/GUI/ForgotPasswordGUI.java
+++ src/GUI/ForgotPasswordGUI.java
@@ -7,13 +7,30 @@ import javax.swing.JTextField;
 
 import entities.Company;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.JSeparator;
+import javax.swing.border.LineBorder;
 
 public class ForgotPasswordGUI {
 
-	private JFrame frame;
+	private JFrame frmForgotPassword;
 	private JTextField txtUsername;
 	private JTextField txtEmail;
 	private JTextField txtCode;
@@ -21,6 +38,19 @@ public class ForgotPasswordGUI {
 	private JTextField txtConfirmPassword;
 	private JButton btnNewButton_4;
 	private Company theCompany;
+	private JPanel panel;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	private JSeparator separator;
+	private JSeparator separator_1;
+	private JSeparator separator_2;
+	private JSeparator separator_3;
+	private JSeparator separator_4;
+	private JLabel lblNewLabel_2;
+	private JLabel lblNewLabel_3;
+	private JLabel lblNewLabel_4;
+	private JLabel lblNewLabel_5;
+	private JLabel lblNewLabel_6;
 
 	/**
 	 * Launch the application.
@@ -47,67 +77,211 @@ public class ForgotPasswordGUI {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize(Company aCompany) {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 566, 452);
-		frame.setVisible(true);
-		frame.setResizable(false);
-		frame.getContentPane().setLayout(null);
+		frmForgotPassword = new JFrame();
+		frmForgotPassword.setTitle("Forgot Password?");
+		frmForgotPassword.getContentPane().setBackground(new Color(255, 153, 102));
+		frmForgotPassword.setBounds(100, 100, 603, 401);
+		//Appears in the center of the screen
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		frmForgotPassword.setLocation(dim.width/2-frmForgotPassword.getSize().width/2, dim.height/2-frmForgotPassword.getSize().height/2);
+		frmForgotPassword.setVisible(true);
+		frmForgotPassword.setResizable(false);
+		frmForgotPassword.getContentPane().setLayout(null);
 		
 		theCompany = aCompany;
 		
 		txtUsername = new JTextField();
+		txtUsername.setBorder(null);
+		txtUsername.setBackground(new Color(255, 153, 102));
+		txtUsername.setForeground(new Color(255, 255, 255));
 		txtUsername.setText("Username");
-		txtUsername.setBounds(204, 82, 116, 22);
-		frame.getContentPane().add(txtUsername);
+		//When the mouse click on the TextField, the text removed
+		txtUsername.addMouseListener(new MouseAdapter(){
+            @Override
+            public void mouseClicked(MouseEvent e){
+                txtUsername.setText("");
+            }
+        });
+		txtUsername.setBounds(380, 33, 116, 14);
+		frmForgotPassword.getContentPane().add(txtUsername);
 		txtUsername.setColumns(10);
 		
 		txtEmail = new JTextField();
+		txtEmail.setBorder(null);
+		txtEmail.setBackground(new Color(255, 153, 102));
+		txtEmail.setForeground(new Color(255, 255, 255));
 		txtEmail.setText("Email");
-		txtEmail.setBounds(204, 117, 116, 22);
-		frame.getContentPane().add(txtEmail);
+		//When the mouse click on the TextField, the text removed
+		txtEmail.addMouseListener(new MouseAdapter(){
+            @Override
+            public void mouseClicked(MouseEvent e){
+            	txtEmail.setText("");
+            }
+        });
+		txtEmail.setBounds(380, 66, 116, 14);
+		frmForgotPassword.getContentPane().add(txtEmail);
 		txtEmail.setColumns(10);
 		
 		JButton btnNewButton = new JButton("Send Verification Code");
-		btnNewButton.setBounds(184, 152, 163, 25);
-		frame.getContentPane().add(btnNewButton);
+		btnNewButton.setForeground(new Color(255, 255, 255));
+		btnNewButton.setBackground(new Color(255, 153, 102));
+		btnNewButton.setBorder(new LineBorder(new Color(255, 255, 255)));
+		btnNewButton.setBounds(355, 99, 163, 25);
+		frmForgotPassword.getContentPane().add(btnNewButton);
 		
 		txtCode = new JTextField();
+		txtCode.setBorder(null);
+		txtCode.setBackground(new Color(255, 153, 102));
+		txtCode.setForeground(new Color(255, 255, 255));
 		txtCode.setText("Code");
-		txtCode.setBounds(184, 190, 69, 22);
-		frame.getContentPane().add(txtCode);
+		//When the mouse click on the TextField, the text removed
+		txtCode.addMouseListener(new MouseAdapter(){
+            @Override
+            public void mouseClicked(MouseEvent e){
+            	txtCode.setText("");
+            }
+        });
+		txtCode.setBounds(351, 152, 69, 14);
+		frmForgotPassword.getContentPane().add(txtCode);
 		txtCode.setColumns(10);
 		
 		JButton btnNewButton_1 = new JButton("Check");
+		btnNewButton_1.setForeground(new Color(255, 255, 255));
+		btnNewButton_1.setBackground(new Color(0, 153, 51));
+		btnNewButton_1.setBorder(new LineBorder(new Color(255, 255, 255)));
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton_1.setBounds(266, 190, 81, 22);
-		frame.getContentPane().add(btnNewButton_1);
+		btnNewButton_1.setBounds(430, 148, 81, 22);
+		frmForgotPassword.getContentPane().add(btnNewButton_1);
 		
 		txtNewPassword = new JTextField();
+		txtNewPassword.setBorder(null);
+		txtNewPassword.setBackground(new Color(255, 153, 102));
+		txtNewPassword.setForeground(new Color(255, 255, 255));
 		txtNewPassword.setText("New Password");
-		txtNewPassword.setBounds(204, 225, 116, 22);
-		frame.getContentPane().add(txtNewPassword);
+		//When the mouse click on the TextField, the text removed
+		txtNewPassword.addMouseListener(new MouseAdapter(){
+            @Override
+            public void mouseClicked(MouseEvent e){
+            	txtNewPassword.setText("");
+            }
+        });
+		txtNewPassword.setBounds(380, 201, 116, 14);
+		frmForgotPassword.getContentPane().add(txtNewPassword);
 		txtNewPassword.setColumns(10);
 		
 		txtConfirmPassword = new JTextField();
+		txtConfirmPassword.setBorder(null);
+		txtConfirmPassword.setBackground(new Color(255, 153, 102));
+		txtConfirmPassword.setForeground(new Color(255, 255, 255));
 		txtConfirmPassword.setText("Confirm Password");
-		txtConfirmPassword.setBounds(204, 260, 116, 22);
-		frame.getContentPane().add(txtConfirmPassword);
+		//When the mouse click on the TextField, the text removed
+		txtConfirmPassword.addMouseListener(new MouseAdapter(){
+            @Override
+            public void mouseClicked(MouseEvent e){
+            	txtConfirmPassword.setText("");
+            }
+        });
+		txtConfirmPassword.setBounds(380, 226, 116, 16);
+		frmForgotPassword.getContentPane().add(txtConfirmPassword);
 		txtConfirmPassword.setColumns(10);
 		
 		JButton btnNewButton_2 = new JButton("Done");
-		btnNewButton_2.setBounds(214, 295, 97, 25);
-		frame.getContentPane().add(btnNewButton_2);
+		btnNewButton_2.setForeground(new Color(255, 255, 255));
+		btnNewButton_2.setBackground(new Color(255, 153, 102));
+		btnNewButton_2.setBorder(new LineBorder(new Color(255, 255, 255)));
+		btnNewButton_2.setBounds(400, 271, 81, 25);
+		frmForgotPassword.getContentPane().add(btnNewButton_2);
 		
 		JButton btnNewButton_3 = new JButton("Log In");
-		btnNewButton_3.setBounds(385, 332, 97, 25);
-		frame.getContentPane().add(btnNewButton_3);
+		btnNewButton_3.setForeground(new Color(255, 255, 255));
+		btnNewButton_3.setBackground(new Color(255, 153, 102));
+		btnNewButton_3.setBorder(new LineBorder(new Color(255, 255, 255)));
+		btnNewButton_3.setBounds(491, 309, 97, 25);
+		frmForgotPassword.getContentPane().add(btnNewButton_3);
 		
 		btnNewButton_4 = new JButton("Help");
-		btnNewButton_4.setBounds(385, 370, 97, 25);
-		frame.getContentPane().add(btnNewButton_4);
+		btnNewButton_4.setForeground(new Color(255, 255, 255));
+		btnNewButton_4.setBackground(new Color(255, 153, 102));
+		btnNewButton_4.setBorder(new LineBorder(new Color(255, 255, 255)));
+		btnNewButton_4.setBounds(491, 336, 97, 25);
+		frmForgotPassword.getContentPane().add(btnNewButton_4);
+		
+		panel = new JPanel();
+		panel.setBackground(new Color(255, 255, 255));
+		panel.setBounds(0, 0, 276, 372);
+		frmForgotPassword.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		lblNewLabel = new JLabel("Forgot your Password?");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
+		lblNewLabel.setBounds(51, 37, 168, 22);
+		panel.add(lblNewLabel);
+		
+		lblNewLabel_1 = new JLabel("");
+		BufferedImage imageicon;
+		try {
+			imageicon = ImageIO.read(new File("label_backgrounds/BSNlogo.jpg"));
+			ImageIcon image = new ImageIcon(imageicon);
+			Image imagerisize = image.getImage().getScaledInstance(120, 120, 90) ;
+			lblNewLabel_1.setIcon(new ImageIcon(imagerisize));
+		} catch (IOException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		lblNewLabel_1.setBounds(69, 100, 150, 120);
+		panel.add(lblNewLabel_1);
+		
+		separator = new JSeparator();
+		separator.setForeground(new Color(255, 255, 255));
+		separator.setBounds(380, 53, 81, 2);
+		frmForgotPassword.getContentPane().add(separator);
+		
+		separator_1 = new JSeparator();
+		separator_1.setForeground(new Color(255, 255, 255));
+		separator_1.setBounds(380, 86, 81, 2);
+		frmForgotPassword.getContentPane().add(separator_1);
+		
+		separator_2 = new JSeparator();
+		separator_2.setBounds(351, 165, 63, 9);
+		frmForgotPassword.getContentPane().add(separator_2);
+		
+		separator_3 = new JSeparator();
+		separator_3.setForeground(new Color(255, 255, 255));
+		separator_3.setBounds(380, 242, 116, 7);
+		frmForgotPassword.getContentPane().add(separator_3);
+		
+		separator_4 = new JSeparator();
+		separator_4.setForeground(Color.WHITE);
+		separator_4.setBounds(380, 215, 116, 7);
+		frmForgotPassword.getContentPane().add(separator_4);
+		
+		lblNewLabel_2 = new JLabel("New label");
+		lblNewLabel_2.setIcon(new ImageIcon("label_backgrounds/user_32px.png"));
+		lblNewLabel_2.setBounds(343, 33, 27, 22);
+		frmForgotPassword.getContentPane().add(lblNewLabel_2);
+		
+		lblNewLabel_3 = new JLabel("New label");
+		lblNewLabel_3.setIcon(new ImageIcon("label_backgrounds/gmail_32px.png"));
+		lblNewLabel_3.setBounds(343, 66, 33, 22);
+		frmForgotPassword.getContentPane().add(lblNewLabel_3);
+		
+		lblNewLabel_4 = new JLabel("New label");
+		lblNewLabel_4.setIcon(new ImageIcon("label_backgrounds/password_32px.png"));
+		lblNewLabel_4.setBounds(314, 148, 27, 22);
+		frmForgotPassword.getContentPane().add(lblNewLabel_4);
+		
+		lblNewLabel_5 = new JLabel("New label");
+		lblNewLabel_5.setIcon(new ImageIcon("label_backgrounds/lock_32px.png"));
+		lblNewLabel_5.setBounds(343, 185, 27, 34);
+		frmForgotPassword.getContentPane().add(lblNewLabel_5);
+		
+		lblNewLabel_6 = new JLabel("New label");
+		lblNewLabel_6.setIcon(new ImageIcon("label_backgrounds/verified_account_32px.png"));
+		lblNewLabel_6.setBounds(343, 223, 27, 22);
+		frmForgotPassword.getContentPane().add(lblNewLabel_6);
 	}
 
 }
