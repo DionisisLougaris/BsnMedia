b9ab1e27270942c8a6649e2b7ab28fd4a51741ee
Minas-Theodoros Charakopoulos <ics20072@uom.edu.gr>
2021-06-13 19:38:46 +0300

    Group Profile Completed.

--- src/GUI/GroupProfileGUI.java
+++ src/GUI/GroupProfileGUI.java
@@ -20,6 +20,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -31,6 +32,7 @@ import java.awt.Cursor;
 
 import javax.imageio.ImageIO;
 import javax.swing.Box;
+import javax.swing.DefaultListModel;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
@@ -39,62 +41,46 @@ import java.awt.SystemColor;
 import javax.swing.JSlider;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+import javax.swing.JSeparator;
+import javax.swing.JList;
+import javax.swing.border.LineBorder;
 
 public class GroupProfileGUI {
 
 	private JFrame frame;
-	private JTextField textField;
+	private JTextField searchField;
 	private static User myUser;
 	private static Group myGroup;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GroupProfileGUI window = new GroupProfileGUI(myUser,myGroup);
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
-	/**
-	 * Create the application.
-	 * @throws IOException 
-	 */
 	public GroupProfileGUI(User myUser,Group myGroup) throws IOException {
-		this.myUser = myUser;
-		this.myGroup = myGroup;
-		initialize();
+		initialize(myUser, myGroup);
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 * @throws IOException 
-	 */
-	private void initialize() throws IOException {
+	private void initialize(User user, Group group) throws IOException {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.WHITE);
 		frame.setBounds(100, 100, 893, 1020);
 		frame.setLocation(500, 0);
 		frame.getContentPane().setLayout(null);
 		frame.setVisible(true);
-		textField = new JTextField();
-		textField.setBackground(new Color(255, 250, 240));
-		textField.setBounds(401, 33, 286, 26);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		searchField = new JTextField();
+		searchField.setBackground(new Color(255, 250, 240));
+		searchField.setBounds(293, 40, 286, 26);
+		frame.getContentPane().add(searchField);
+		searchField.setColumns(10);
 		
-		JLabel lblNewLabel = new JLabel();
-		lblNewLabel.setText(myGroup.getName());
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 27));
-		lblNewLabel.setBounds(38, 122, 274, 54);
-		frame.getContentPane().add(lblNewLabel);
+		myUser = user;
+		myGroup = group;
+		
+		JLabel groupNameLabel = new JLabel();
+		groupNameLabel.setForeground(new Color(255, 0, 0));
+		groupNameLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		groupNameLabel.setText(myGroup.getName());
+		groupNameLabel.setFont(new Font("Tahoma", Font.ITALIC, 28));
+		groupNameLabel.setBounds(38, 122, 825, 54);
+		frame.getContentPane().add(groupNameLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Project in Progress:");
 		lblNewLabel_1.setBounds(38, 220, 125, 16);
@@ -108,22 +94,31 @@ public class GroupProfileGUI {
 		lblNewLabel_3.setBounds(38, 278, 117, 16);
 		frame.getContentPane().add(lblNewLabel_3);
 		
-		JLabel lblNewLabel_4 = new JLabel("Group Evaluation:");
-		lblNewLabel_4.setBounds(38, 350, 117, 16);
+		JTextArea projectDesc = new JTextArea(myGroup.getMyProject().getDescription());
+		projectDesc.setBackground(new Color(255, 250, 240));
+		projectDesc.setEditable(false);
+		projectDesc.setLineWrap(true);
+		projectDesc.setBorder(new LineBorder(new Color(0, 0, 0)));
+		projectDesc.setBounds(189, 279, 390, 46);
+		frame.getContentPane().add(projectDesc);
+		
+		JLabel lblNewLabel_4 = new JLabel("Group Evaluation: (/100)");
+		lblNewLabel_4.setBounds(38, 350, 141, 16);
 		frame.getContentPane().add(lblNewLabel_4);
 		
 		JSlider slider = new JSlider();
+		slider.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		slider.setOpaque(false);
 		slider.setBackground(Color.WHITE);
-		slider.setBounds(240, 350, 200, 26);
+		slider.setBounds(189, 346, 200, 26);
 		frame.getContentPane().add(slider);
 		
-		JButton btnNewButton_2 = new JButton("Submit");
-		btnNewButton_2.setContentAreaFilled(false); 
-		btnNewButton_2.setFocusPainted(false); 
-		btnNewButton_2.setOpaque(false);
-		btnNewButton_2.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		btnNewButton_2.addActionListener(new ActionListener() {
+		JButton submitRatingButton = new JButton("Submit");
+		submitRatingButton.setContentAreaFilled(false); 
+		submitRatingButton.setFocusPainted(false); 
+		submitRatingButton.setOpaque(false);
+		submitRatingButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		submitRatingButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				if(myUser instanceof Boss)
 				{
@@ -155,64 +150,66 @@ public class GroupProfileGUI {
 				}
 			}
 		});
-		btnNewButton_2.setBounds(452, 346, 86, 25);
-		frame.getContentPane().add(btnNewButton_2);
+		submitRatingButton.setBounds(401, 346, 86, 25);
+		frame.getContentPane().add(submitRatingButton);
 		
-		JLabel lblNewLabel_5 = new JLabel("Members of Group");
-		lblNewLabel_5.setBounds(38, 447, 115, 16);
+		JLabel lblNewLabel_5 = new JLabel("Members of Group("+myGroup.getGroupMembers().size()+")");
+		lblNewLabel_5.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_5.setBounds(38, 447, 190, 16);
 		frame.getContentPane().add(lblNewLabel_5);
 		
 		JLabel lblNewLabel_6 = new JLabel("Posts of the Group");
-		lblNewLabel_6.setBounds(323, 447, 117, 16);
+		lblNewLabel_6.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_6.setBounds(323, 447, 506, 16);
 		frame.getContentPane().add(lblNewLabel_6);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(40, 492, 193, 306);
-		frame.getContentPane().add(panel);
-		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(322, 489, 507, 393);
+		panel_1.setBounds(323, 474, 506, 408);
 		frame.getContentPane().add(panel_1);
 		panel_1.setLayout(null);
 		
-		JTextArea textArea = new JTextArea();
+		JTextArea postTextArea = new JTextArea();
 		for( Post post : myGroup.getGroupPosts())
 		{
 			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 	        String formatDateTime = post.getTimestamp().format(formatter);
-	        textArea.append("------------------------------------------------------------------------------------------------------------"+ ""+ "\n\r");
-	        textArea.append(post.getContent()+" | "+post.getCreator().getFirstName()+" | "+formatDateTime+ "\n\r");
+	        postTextArea.append("------------------------------------------------------------------------------------------------------------"+ ""+ "\n\r");
+	        postTextArea.append(post.getContent()+" | "+post.getCreator().getFirstName()+" | "+formatDateTime+ "\n\r");
 		}
-		textArea.setBackground(new Color(255, 250, 240));
-		textArea.setBounds(0, 0, 507, 393);
-		panel_1.add(textArea);
+		postTextArea.setBackground(new Color(255, 250, 240));
+		postTextArea.setEditable(false);
+		postTextArea.setLineWrap(true);
+		postTextArea.setBounds(0, 0, 507, 408);
+		panel_1.add(postTextArea);
 		
-		JLabel lblNewLabel_7 = new JLabel("/100");
-		lblNewLabel_7.setBackground(SystemColor.text);
-		lblNewLabel_7.setBounds(185, 350, 48, 16);
-		frame.getContentPane().add(lblNewLabel_7);
+		JLabel projectName = new JLabel();
+		projectName.setText(myGroup.getMyProject().getName());
+		projectName.setBounds(187, 220, 457, 16);
+		frame.getContentPane().add(projectName);
 		
-		JLabel lblNewLabel_8 = new JLabel();
-		lblNewLabel_8.setText(myGroup.getMyProject().getName());
-		lblNewLabel_8.setBounds(187, 220, 125, 16);
-		frame.getContentPane().add(lblNewLabel_8);
+		JLabel projectSituation = new JLabel();
+		projectSituation.setText(myGroup.getMyProject().getStatus());
+		projectSituation.setBounds(185, 249, 125, 16);
+		frame.getContentPane().add(projectSituation);
 		
-		JLabel lblNewLabel_9 = new JLabel();
-		lblNewLabel_9.setText(myGroup.getMyProject().getStatus());
-		lblNewLabel_9.setBounds(185, 249, 56, 16);
-		frame.getContentPane().add(lblNewLabel_9);
-		
-		JLabel lblNewLabel_10 = new JLabel();
-		lblNewLabel_10.setText(myGroup.getMyProject().getDescription());
-		lblNewLabel_10.setBounds(187, 278, 507, 16);
-		frame.getContentPane().add(lblNewLabel_10);
-		
-		JButton btnNewButton_1_1 = new JButton("logo");
-		btnNewButton_1_1.setContentAreaFilled(false); 
-		btnNewButton_1_1.setFocusPainted(false); 
-		btnNewButton_1_1.setOpaque(false);
-		btnNewButton_1_1.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		btnNewButton_1_1.addActionListener(new ActionListener() {
+		BufferedImage buttonIcon = null;
+		try {
+			buttonIcon = ImageIO.read(new File("Buttons_backgrounds/BSN-logo-white.png"));
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		ImageIcon image = new ImageIcon(buttonIcon);
+		Image imagerisizel = image.getImage().getScaledInstance(80, 45, 60) ;
+		ImageIcon imagebutton = new ImageIcon(imagerisizel);
+	    JButton goBackButton = new JButton(imagebutton);
+		goBackButton.setContentAreaFilled(false); 
+		goBackButton.setFocusPainted(false); 
+		goBackButton.setBorderPainted(false);
+		goBackButton.setToolTipText("Go back to your Profile");
+		goBackButton.setOpaque(false);
+		goBackButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		goBackButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//Closing previous GUI
 				frame.setVisible(false);
@@ -246,41 +243,86 @@ public class GroupProfileGUI {
 				}
 			}
 		});
-		btnNewButton_1_1.setBounds(38, 20, 62, 53);
-		frame.getContentPane().add(btnNewButton_1_1);
+		goBackButton.setBounds(20, 20, 80, 66);
+		frame.getContentPane().add(goBackButton);
 		
 		Icon search = new ImageIcon("Buttons_backgrounds/search_30px.png");
-		JButton btnNewButton_1 = new JButton(search);
-		btnNewButton_1.setContentAreaFilled(false); 
-		btnNewButton_1.setFocusPainted(false); 
-		btnNewButton_1.setOpaque(false);
-		btnNewButton_1.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		btnNewButton_1.addActionListener(new ActionListener() {
+		JButton searchButton = new JButton(search);
+		searchButton.setContentAreaFilled(false); 
+		searchButton.setFocusPainted(false); 
+		searchButton.setOpaque(false);
+		searchButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		searchButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
+				String searchedText = searchField.getText();
+				if(!searchedText.isEmpty()) {
+					boolean result;
+					try {
+						result = myUser.getMyAccount().getMyCompany().searchObject(searchedText, myUser);
+
+						if (!result) {
+							ArrayList<String> suggestedOptions = new ArrayList<String>();
+							new SearchSuggestionsGUI(suggestedOptions, myUser);
+						}else {
+							frame.setVisible(false);
+						}
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}else {
+					 String message = "Type something in the Search field";
+						JOptionPane.showMessageDialog(new JFrame(), message, "Message",
+						        JOptionPane.INFORMATION_MESSAGE);
+				}
 			}
 		});
-		btnNewButton_1.setBounds(696, 33, 55, 39);
-		frame.getContentPane().add(btnNewButton_1);
+		searchButton.setBounds(589, 27, 55, 39);
+		frame.getContentPane().add(searchButton);
 		
 		Icon help = new ImageIcon("Buttons_backgrounds/customer_support_40px.png");
-		JButton btnNewButton_1_1_1 = new JButton(help);
-		btnNewButton_1_1_1.setContentAreaFilled(false); 
-		btnNewButton_1_1_1.setFocusPainted(false); 
-		btnNewButton_1_1_1.setOpaque(false);
-		btnNewButton_1_1_1.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		btnNewButton_1_1_1.addActionListener(new ActionListener() {
+		JButton helpButton = new JButton(help);
+		helpButton.setContentAreaFilled(false); 
+		helpButton.setFocusPainted(false); 
+		helpButton.setOpaque(false);
+		helpButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		helpButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
+				try {
+					frame.setVisible(false);
+					new HelpGUI(myUser);
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 			}
 		});
-		btnNewButton_1_1_1.setBounds(817, 922, 46, 38);
-		frame.getContentPane().add(btnNewButton_1_1_1);
+		helpButton.setBounds(808, 922, 55, 48);
+		frame.getContentPane().add(helpButton);
+		
+		JSeparator separator = new JSeparator();
+		separator.setForeground(Color.WHITE);
+		separator.setBounds(0, 174, 877, 16);
+		frame.getContentPane().add(separator);
+		
+		JList<String> membersList = new JList<String>();
+		DefaultListModel<String> listModel = new DefaultListModel<String>();
+		for (Employee theMember: myGroup.getGroupMembers()) {
+			listModel.addElement(theMember.getFirstName()+" "+theMember.getLastName()+" | "+theMember.getMyAccount().getUsername());
+		}
+		membersList.setModel(listModel);
+		membersList.setBackground(new Color(255, 250, 240));
+		membersList.setBounds(38, 474, 190, 408);
+		frame.getContentPane().add(membersList);
 		
 		JLabel lblNewLabel_11 = new JLabel("");
 		BufferedImage imagebackground = ImageIO.read(new File("label_backgrounds/background.jpg"));
 		ImageIcon imageb = new ImageIcon(imagebackground);
 		Image imagerisizeb = imageb.getImage().getScaledInstance(887, 991, 140) ;
 		lblNewLabel_11.setIcon(new ImageIcon(imagerisizeb));
-		lblNewLabel_11.setBounds(0, 0, 875, 973);
+		lblNewLabel_11.setBounds(0, 0, 875, 981);
 		frame.getContentPane().add(lblNewLabel_11);
 	}
 }
