160690ef720c0c1291c5322a4d118795ccf1eff7
Dionisis Lougaris <dlougaris@gmail.com>
2021-06-14 19:00:15 +0300

    Additions: Giving functionality to conversation GUIs

--- src/GUI/GroupChatGUI.java
+++ src/GUI/GroupChatGUI.java
@@ -11,35 +11,32 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.event.ActionListener;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Dimension;
+import javax.swing.JTextArea;
+
+import entities.Group;
+import entities.Message;
+import entities.Storage;
+import entities.User;
 
 public class GroupChatGUI {
 
 	private JFrame frame;
+	private User myUser;
+	private Group myGroup;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GroupChatGUI window = new GroupChatGUI();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
+	
 	/**
 	 * Create the application.
 	 */
-	public GroupChatGUI() {
+	public GroupChatGUI(Group myGroup,User myUser) {
+		this.myUser= myUser;
+		this.myGroup = myGroup;
 		initialize();
 	}
 
@@ -49,7 +46,7 @@ public class GroupChatGUI {
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.WHITE);
-		frame.setBounds(100, 100, 685, 840);
+		frame.setBounds(100, 100, 686, 794);
 		frame.getContentPane().setLayout(null);
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
@@ -69,7 +66,7 @@ public class GroupChatGUI {
 		
 		JLabel lblNewLabel = new JLabel("Your messages are encrypted");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel.setBounds(80, 9, 215, 32);
+		lblNewLabel.setBounds(80, 15, 215, 20);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Group Conversation of \" \"");
@@ -87,13 +84,35 @@ public class GroupChatGUI {
 		panel_1.setBackground(new Color(255, 250, 240));
 		panel_1.setBounds(12, 13, 619, 493);
 		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
+		textArea.setBounds(0, 0, 619, 493);
+		ArrayList<String> ourMessages = new ArrayList<>();
+		ourMessages= Storage.retrieveConversation(myGroup.getMyConversation());
+
+			for(int i=0;i<ourMessages.size();i++)
+			{
+				textArea.append(ourMessages.get(i)+ "\n\r");
+			}
+			
+		panel_1.add(textArea);
 		
 		JPanel panel_2 = new JPanel();
 		panel_2.setBackground(new Color(255, 250, 240));
-		panel_2.setBounds(12, 519, 532, 120);
+		panel_2.setBounds(12, 519, 521, 120);
 		panel.add(panel_2);
+		panel_2.setLayout(null);
 		
-		Icon send = new ImageIcon("Buttons_backgrounds/email_send_32px.png");
+		JTextArea textArea_1 = new JTextArea();
+		textArea_1.setLineWrap(true);
+		textArea_1.setWrapStyleWord(true);
+		textArea_1.setBounds(0, 0, 521, 120);
+		panel_2.add(textArea_1);
+		
+		Icon send = new ImageIcon("Buttons_backgrounds/email_send_50px.png");
 		JButton btnNewButton_1_1_1 = new JButton(send);
 		btnNewButton_1_1_1.setContentAreaFilled(false); 
 		btnNewButton_1_1_1.setFocusPainted(false); 
@@ -101,9 +120,17 @@ public class GroupChatGUI {
 		btnNewButton_1_1_1.setCursor(new Cursor(Cursor.HAND_CURSOR));
 		btnNewButton_1_1_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
+				String myText = textArea_1.getText();
+				Message myMessage = new Message(myText,myUser);
+				myGroup.getMyConversation().addMessage(myMessage);
+				textArea.setText(""); 
+				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		        String formatDateTime = myMessage.getTimesent().format(formatter);
+				textArea.append(myMessage.getContent()+" | Date:"+formatDateTime+" | "+myMessage.getSender().getMyAccount().getUsername()+" | ");
 			}
 		});
-		btnNewButton_1_1_1.setBounds(572, 601, 46, 38);
+		btnNewButton_1_1_1.setBounds(567, 566, 52, 52);
 		panel.add(btnNewButton_1_1_1);
 		
 		Icon help = new ImageIcon("Buttons_backgrounds/customer_support_40px.png");
--- src/GUI/PrivateChatGUI.java
+++ src/GUI/PrivateChatGUI.java
@@ -6,6 +6,9 @@ import javax.swing.JFrame;
 import javax.swing.JTextField;
 
 import entities.Conversation;
+import entities.Message;
+import entities.Post;
+import entities.Storage;
 import entities.User;
 
 import javax.swing.Icon;
@@ -21,7 +24,10 @@ import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
 
 public class PrivateChatGUI {
 
@@ -61,7 +67,7 @@ public class PrivateChatGUI {
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.WHITE);
-		frame.setBounds(100, 100, 685, 840);
+		frame.setBounds(100, 100, 686, 795);
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
 		frame.getContentPane().setLayout(null);
@@ -88,7 +94,7 @@ public class PrivateChatGUI {
 		
 		JLabel lblNewLabel = new JLabel("Your messages are encrypted");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel.setBounds(70, 23, 215, 16);
+		lblNewLabel.setBounds(70, 23, 215, 20);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Private Conversation with "+receiver.getFirstName());
@@ -105,12 +111,37 @@ public class PrivateChatGUI {
 		JPanel panel_1 = new JPanel();
 		panel_1.setBounds(12, 13, 619, 493);
 		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
+		textArea.setText("");
+		
+		ArrayList<String> ourMessages = new ArrayList<>();
+		ourMessages= Storage.retrieveConversation(convo);
+
+			for(int i=0;i<ourMessages.size();i++)
+			{
+				textArea.append(ourMessages.get(i)+ "\n\r");
+			}
+		
+		textArea.setBackground(Color.WHITE);
+		textArea.setBounds(0, 0, 619, 493);
+		panel_1.add(textArea);
 		
 		JPanel panel_2 = new JPanel();
 		panel_2.setBounds(12, 519, 521, 120);
 		panel.add(panel_2);
+		panel_2.setLayout(null);
 		
-		Icon send = new ImageIcon("Buttons_backgrounds/email_send_32px.png");
+		JTextArea textArea_1 = new JTextArea();
+		textArea_1.setLineWrap(true);
+		textArea_1.setWrapStyleWord(true);
+		textArea_1.setBounds(0, 0, 521, 120);
+		panel_2.add(textArea_1);
+		
+		Icon send = new ImageIcon("Buttons_backgrounds/email_send_50px.png");
 		JButton btnNewButton_1_1_1 = new JButton(send);
 		btnNewButton_1_1_1.setContentAreaFilled(false); 
 		btnNewButton_1_1_1.setFocusPainted(false); 
@@ -118,9 +149,17 @@ public class PrivateChatGUI {
 		btnNewButton_1_1_1.setCursor(new Cursor(Cursor.HAND_CURSOR));
 		btnNewButton_1_1_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				String myText = textArea_1.getText();
+				Message myMessage = new Message(myText,sender);
+				convo.addMessage(myMessage);
+				textArea.setText(""); 
+				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		        String formatDateTime = myMessage.getTimesent().format(formatter);
+				textArea.append(myMessage.getContent()+" | Date:"+formatDateTime+" | "+myMessage.getSender().getMyAccount().getUsername()+" | ");
+
 			}
 		});
-		btnNewButton_1_1_1.setBounds(569, 601, 46, 38);
+		btnNewButton_1_1_1.setBounds(567, 569, 52, 52);
 		panel.add(btnNewButton_1_1_1);
 		
 		Icon help = new ImageIcon("Buttons_backgrounds/customer_support_40px.png");
